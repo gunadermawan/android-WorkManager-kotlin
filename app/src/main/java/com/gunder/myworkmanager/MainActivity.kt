538847f97c52e1917840d6eeda4bfd71217a22a3
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-10 05:32:49 +0700

    membuat fungsi peridodiktask

--- app/src/main/java/com/gunder/myworkmanager/MainActivity.kt
+++ app/src/main/java/com/gunder/myworkmanager/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.work.*
 import com.gunder.myworkmanager.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var workManager: WorkManager
     private lateinit var binding: ActivityMainBinding
+    private lateinit var periodicWorkRequest: PeriodicWorkRequest
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,12 +18,45 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         workManager = WorkManager.getInstance(this)
         binding.btnOneTimeTask.setOnClickListener(this)
+        binding.btnPeriodikTask.setOnClickListener(this)
+        binding.btnCancelPeriodikTask.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
+        when (v?.id) {
             R.id.btn_one_time_task -> startOneTimeTask()
+            R.id.btn_periodik_task -> startPeriodicTask()
+            R.id.btn_cancel_periodik_task -> cancelPeriodicTask()
         }
+    }
+
+    fun startPeriodicTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        periodicWorkRequest =
+            PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build()
+        workManager.enqueue(periodicWorkRequest)
+        workManager.getWorkInfoByIdLiveData(periodicWorkRequest.id)
+            .observe(this@MainActivity, { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+                binding.btnCancelPeriodikTask.isEnabled = false
+                if (workInfo.state == WorkInfo.State.ENQUEUED) {
+                    binding.btnCancelPeriodikTask.isEnabled = true
+                }
+            })
+    }
+
+    private fun cancelPeriodicTask() {
+        workManager.cancelWorkById(periodicWorkRequest.id)
     }
 
     private fun startOneTimeTask() {
@@ -29,7 +64,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val data = Data.Builder()
             .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
             .build()
-        val constraint = Constraints.Builder()
+        val  constraint = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
         val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
@@ -37,7 +72,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             .build()
         workManager.enqueue(oneTimeWorkRequest)
         workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
-            .observe(this@MainActivity,{workInfo ->
+            .observe(this@MainActivity, { workInfo ->
                 val status = workInfo.state.name
                 binding.textStatus.append("\n" + status)
             })
